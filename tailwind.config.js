651c28c544f529bfe1f6cd95ba07a3a40535b726
poompichayout <poompichayout.kon@dome.tu.ac.th>
2022-08-13 10:54:57 +0700

    finished project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./scripts/**/*.js",
+    "./styles/**/*.css",
+    "./index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
